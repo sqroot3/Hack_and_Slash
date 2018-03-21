358bbbb1c98215f8d5563131c6a483a6df34ff68
Matias L <mlwin10@hotmail.com>
2018-03-21 15:28:44 -0400

    Added dialog functionality. Each character/entity which would have a dialog would need to have a Dialog component attached to it, which can then be called from whatever script the dialog is related to with a call like (reference to char/entity dialog script).WriteMessage(1f, 1f, "You shall not pass", true);

---
+++ Assets/Scripts/UI/Dialog.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text), typeof(Canvas))]
+public class Dialog : MonoBehaviour {
+    [SerializeField] private Text message;
+    [SerializeField] private Canvas UICanvas;
+    [SerializeField] private Sprite character;
+    [SerializeField] private Image avatar;
+    public float charTime = 1.0f;
+    public float expiringTime = 5.0f;
+    private float[] positionLeft = { -204f, 4.5f };
+    private float[] positionRight = { 214f, 4.5f };
+
+
+    void Update () {
+		//Debug-test code
+
+        //toggle canvas test
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            UICanvas.enabled = !UICanvas.enabled;
+        }
+
+        //write message test
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            StartCoroutine(WriteMessage(charTime, expiringTime, "Hello there", true));
+        }
+        
+	}
+
+    public IEnumerator WriteMessage(float charTime, float expiringTime, string text, bool isLeft)
+    {
+        RectTransform parent = avatar.GetComponentInParent<RectTransform>();
+        parent.anchoredPosition3D = (isLeft) ? new Vector3(positionLeft[0], positionLeft[1]) : new Vector3(positionRight[0], positionRight[1]);
+        message.text = "";
+        UICanvas.enabled = true;
+        avatar.sprite = character;
+        for (int i = 0; i < text.Length; ++i)
+        {
+            message.text += text[i];
+            //wait for char time
+            yield return new WaitForSeconds(charTime);
+        }
+        StartCoroutine(DeleteMessage(expiringTime));
+    }
+
+    public IEnumerator DeleteMessage(float expiringTime)
+    {
+        yield return new WaitForSeconds(expiringTime);
+        message.text = "";
+        UICanvas.enabled = false;
+    }
+}
