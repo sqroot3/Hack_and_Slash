fb7a8de6a339ff8fa266140f32287a7f8c807c96
Matias L <mlwin10@hotmail.com>
2018-03-23 23:51:51 -0400

    Added multiline dialog support (some words are still being cut-off, and some spaces are not formatted properly - will try to fix this next)

--- Assets/Scripts/UI/Dialog.cs
+++ Assets/Scripts/UI/Dialog.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Dialog : MonoBehaviour {
     [SerializeField] private Text message;
@@ -13,6 +14,7 @@ public class Dialog : MonoBehaviour {
     public float expiringTime = 5.0f;
     private float[] positionLeft = { -204f, 4.5f };
     private float[] positionRight = { 214f, 4.5f };
+    private int charLimit = 59;
     
 
 
@@ -37,9 +39,17 @@ public class Dialog : MonoBehaviour {
             UICanvas.enabled = false;
             StopAllCoroutines();
         }
+
+        //long message test
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            string longText = "Bacon ipsum dolor amet jowl biltong ham hock turducken trifecta. Flank beef rump, pork loin landjaeger tenderloin biltong turkey short loin pork chop meatball. Doner meatball pig short ribs pancetta. Leberkas fatback pork belly, frankfurter alcatra beef ribs shank meatloaf hamburger. Tenderloin venison shank brisket short ribs frankfurter pork chop hamburger. Tenderloin strip steak bresaola, meatball salami sirloin boudin short ribs meatloaf. Swine prosciutto pork loin short ribs shankle.";
+            StartCoroutine(WriteMessage(charTime, expiringTime, longText, false));
+        }
         
 	}
 
+    
     public IEnumerator WriteMessage(float charTime, float expiringTime, string text, bool isLeft)
     {
         RectTransform parent = avatar.GetComponentInParent<RectTransform>();
@@ -47,12 +57,25 @@ public class Dialog : MonoBehaviour {
         message.text = "";
         UICanvas.enabled = true;
         avatar.sprite = character;
-        for (int i = 0; i < text.Length; ++i)
+
+        //take into account longer lines - may need to subdivide
+        //when we've printed 3 lines already, message needs to be adjusted to include the previous 2 as the start, then print the current, and so on
+        List<String> lines = SplitString(text);
+        
+        //for each line, print each character
+        for(int line = 0; line < lines.Count; ++line)
         {
-            message.text += text[i];
-            //wait for char time
-            yield return new WaitForSeconds(charTime);
+            if(line >= 2)
+            {
+                message.text = lines[line - 2] + lines[line - 1];
+            }
+            for(int i = 0; i < lines[line].Length; ++i)
+            {
+                message.text += lines[line][i];
+                yield return new WaitForSeconds(charTime);
+            }
         }
+
         StartCoroutine(DeleteMessage(expiringTime));
     }
 
@@ -62,4 +85,30 @@ public class Dialog : MonoBehaviour {
         message.text = "";
         UICanvas.enabled = false;
     }
+
+    //Returns a list of substrings generated from the initial string
+    public List<String> SplitString(string text)
+    {
+        List<String> dst = new List<String>();
+        dst.Add("");
+
+        int lineNumber = 0;
+        for (int i = 0; i < text.Length; ++i)
+        {
+            //if at char limit, go to new line
+            if (i % charLimit == 0 && i != 0)
+            {
+                dst.Add("");
+                //if word isn't completed, add on a hyhon
+                if (Char.IsWhiteSpace(text[i + 1]) || Char.IsWhiteSpace(text[i]) || Char.IsWhiteSpace(text[i - 1]))
+                    dst[lineNumber++] += "\n";
+                else
+                    dst[lineNumber++] += "-\n";
+            }
+
+            dst[lineNumber] += text[i];
+        }
+        return dst;
+    }
+
 }
