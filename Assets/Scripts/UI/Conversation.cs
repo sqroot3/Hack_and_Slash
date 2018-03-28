19d21898b86b487a06d6654510f5ead782bc268b
Matias <argfoo.code@gmail.com>
2018-03-27 20:37:05 -0400

    Fixed incorrect approach to Dialog system - Use scriptable objects (do one for Speaker - hold id and avatar)

---
+++ Assets/Scripts/UI/Conversation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//will live as asset in project
+
+[CreateAssetMenu(fileName = "Conversation", menuName = "Dialog")]
+public class Conversation : ScriptableObject {
+
+    [Serializable]
+	public class ConversationMessage
+    {
+        public int speaker;
+        [Multiline]
+        public string message;
+        public Sprite avatar;
+    }
+
+    public List<ConversationMessage> messages;
+}
---
+++ Assets/Scripts/UI/ConversationPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConversationPlayer : MonoBehaviour {
+
+    public Conversation conversation;
+
+    public Text text;
+    public Image image;
+
+
+    private int currentMessage = 0;
+
+	// Use this for initialization
+	void Start () {
+        currentMessage = 0;
+        SetMessage(currentMessage);
+	}
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SetMessage(currentMessage++);
+        }
+    }
+
+    void SetMessage(int messageNumber)
+    {
+        text.text = conversation.messages[messageNumber].message;
+        setSpeakerPic(conversation.messages[messageNumber].speaker);
+    }
+	
+	void setSpeakerPic(int speaker)
+    {
+        switch(speaker)
+        {
+            case 0:
+                image.material.color = Color.red;
+                break;
+            case 1:
+                image.material.color = Color.blue;
+                break;
+            default:
+                image.material.color = Color.white;
+                break;
+        }
+    }
+}
