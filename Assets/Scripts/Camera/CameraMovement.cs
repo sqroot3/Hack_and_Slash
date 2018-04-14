57adc3a3cc8833f9342fb49882c56a23ff2f596a
Matias L <mlwin10@hotmail.com>
2018-04-13 23:37:08 -0300

    Fixed camera issue. Now player can freely rotate/move around as intended. A camera rig now has the following customizable properties: 1) Vertical Distance - the vertical distance the camera is away from the player (must be positive) 2) Horizontal Distance - the horizontal distance the camera is away from the player (is implemented as negative - field can be supplied as positive) 3) Lookdown Angle - the angle to which the camera looks down at the player.
    All of these settings should be customizable later on. A settings menu should include these to allow the player to select a comfortable playing experience.

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -7,9 +7,12 @@ public class CameraMovement : MonoBehaviour {
     //This is attached to the Camera Rig
     //Rotations/Movement need to be done to the rig itself not the camera
     //Need "orbiting" effect around player
+    //CameraRig is child to player, hence no need to move it on its own
     [SerializeField] private Transform player;
     [SerializeField] private float sensitivity;
-    [SerializeField] private float minPlayerDistance = 2.80f;
+    [SerializeField] private float verticalDistance = 1.32f;
+    [SerializeField] private float horizontalDistance = 2.68f;
+    [SerializeField] private float lookdownAngle = 17.14f;
 
     private string horizontalAxis = "MouseHorizontal";
     private string verticalAxis = "MouseVertical";
@@ -19,6 +22,9 @@ public class CameraMovement : MonoBehaviour {
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
+        //update camera to be "x" units away and "y" units away from player
+        transform.localPosition = new Vector3(0f, verticalDistance, -horizontalDistance);
+        transform.localRotation = Quaternion.Euler(lookdownAngle, 0f, 0f);
 	}
 	
 	// Update is called once per frame
@@ -36,16 +42,5 @@ public class CameraMovement : MonoBehaviour {
     {
         //Orbit around player by "angle" amount
         player.Rotate(new Vector3(0f, angle,0f));
-        //transform.RotateAround(player.position, Vector3.up, -angle);
-        
-    }
-
-    public void Move(Vector3 velocity)
-    {
-        /*
-        Debug.Log("Distance to player: " + Vector3.Distance(transform.position, player.position));
-        rb.velocity = velocity;
-        */
-        
     }
 }
